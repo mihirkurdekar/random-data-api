5869dd472bd2590fd718a740e76420a0e28ff752
Mihir Kurdekar <mihir.kurdekar@T-mobile.com>
2017-11-13 17:25:39 +0530

    Reading query params

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,24 @@
-import * as express from 'express'
+import * as express from 'express';
+import * as url from "url";
 
 class App {
-  public express
+  public express;
 
   constructor () {
-    this.express = express()
-    this.mountRoutes()
+    this.express = express();
+    this.mountRoutes();
   }
 
   private mountRoutes (): void {
     const router = express.Router()
-    router.get('/', (req, res) => {
+    router.get('/', (req, res) => { 
+      let parsedUrl= url.parse(req.url,true);
       res.json({
-        message: 'Hello World!'
-      })
-    })
-    this.express.use('/', router)
+        message: 'Hello '+parsedUrl.query['name']+' !'
+      });
+    });
+    this.express.use('/', router);
   }
 }
 
-export default new App().express
+export default new App().express;
