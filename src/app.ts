29433855ed9fa1574d2ef11c2fc5d36f574b4e73
Mihir Kurdekar <mihir.kurdekar@T-mobile.com>
2017-11-14 21:58:37 +0530

    Code for random generating random values

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,51 @@
 import * as express from 'express';
 import * as url from "url";
+import * as bodyParser from "body-parser";
+import { GenerateRandomDataService } from "./generate-random-data-service";
+import { ChartData } from './chart-data';
 
 class App {
   public express;
 
-  constructor () {
+  constructor() {
     this.express = express();
+    this.express.use(bodyParser.urlencoded({ extended: true }));
     this.mountRoutes();
   }
 
-  private mountRoutes (): void {
+  private mountRoutes(): void {
     const router = express.Router()
-    router.get('/', (req, res) => { 
-      let parsedUrl= url.parse(req.url,true);
+    //sample get
+    router.get('/get', (req, res) => {
+      let parsedUrl = url.parse(req.url, true);
       res.json({
-        message: 'Hello '+parsedUrl.query['name']+' !'
+        message: 'Hello ' + parsedUrl.query['name'] + ' !'
       });
+    });
+
+    //sample post
+    router.post('/post', (req, res) => {
+      //query param
+      let parsedUrl = url.parse(req.url, true);
+      //body
+      let name: string = req.body['name'];
+      res.json({
+        message: 'Hello ' + name + parsedUrl.query['name'] + ' !'
+      });
+    });
+
+    router.post('/randomdata', (req, res) => {
+      let start: string = req.body['startDate'];
+      let end: string = req.body['endDate'];
+      let response:Array<ChartData> = []
+      if (typeof start != undefined && start != null && end != undefined && end != null) {
+        let startDate: Date = new Date(start);
+        let endDate: Date = new Date(end);
+        console.log(""+startDate+endDate);
+        let generateRandomDataService: GenerateRandomDataService = new GenerateRandomDataService();
+        response = generateRandomDataService.generateData(startDate, endDate);
+      }
+      res.json(response);
     });
     this.express.use('/', router);
   }
